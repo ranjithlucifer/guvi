1aa72aa72dd1c9a2a05d55a29b991a371c1ec353
ranjithlucifer <51074137+ranjithlucifer@users.noreply.github.com>
2019-05-27 21:29:01 +0530

    Update codekataset13q.py

--- codekataset13q.py
+++ codekataset13q.py
@@ -1,3 +1,6 @@
 n=input()
-if n=='a' or n=='e' or n=='i' or n=='o' or n=='u': print("Vowel")
+if (n>='a' and n<='z') or (n>='A' and n<='B'): 
+  if n=='a' or n=='e' or n=='i' or n=='o' or n=='u': print("Vowel")
+  else: print("Consonant")
+elif n>='0' and n<='9': print("Digit")
 else: print("Invalid")
